45a9a67f10db3695b4956f3afe86699301f8f552
Gustavo <Gustavo@Gustavo-PC.mshome.net>
2014-05-11 19:55:44 -0300

    Criada a interface da primeira tela

--- src/util/GUIConstants.java
+++ src/util/GUIConstants.java
@@ -3,10 +3,11 @@ package util;
 public interface GUIConstants {
 
     public final String TITLE = "Jogo da Velha utilizando Sockets";
-    public final int FRAME_WIDTH = 500;
-    public final int FRAME_HEIGHT = 400;
+    public final int FRAME_WIDTH = 800;
+    public final int FRAME_HEIGHT = 600;
     public final int BASE_COMPONENT_WIDTH = (FRAME_WIDTH / 10) * 4;
     public final int BASE_COMPONENT_HEIGHT = FRAME_HEIGHT / 20;
     public final int HORIZONAL_CENTER_POS = FRAME_WIDTH / 2 - BASE_COMPONENT_WIDTH / 2;
+    public final int VERTICAL_CENTER_POS = FRAME_HEIGHT / 2 - BASE_COMPONENT_HEIGHT / 2;
 
 }
